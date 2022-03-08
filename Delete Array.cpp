97ee2dfa8e2a817df3ab269cb32827deebadd3d8
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-03-08 22:48:00 +0530

    Create Delete Array.cpp

---
+++ Delete Array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int testcase;
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+        
+        // Declaring dynamic 1D array
+        int *arr = new int[N];
+        
+        for(int i = 0;i<N;i++){
+            cin >> arr[i];
+        }
+        
+        int sum = 0;
+	    for(int i = 0;i<N;i++){
+	       sum += arr[i];
+	    }
+	    
+	    
+     //delete[] arr;     
+	    cout << sum << endl;
+	    
+	    cout << arr[0] << endl;
+	}
+	
+	return 0;
+}  
